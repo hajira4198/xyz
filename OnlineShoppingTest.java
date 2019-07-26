090eb3bd750215794b7168e904551ba1852a1ec0
training_d2.03.07 <training_d2.03.07@accenture.com>
2019-07-26 17:47:45 +0530

    gdhjytguky

---
+++ OnlineShoppingTest.java
@@ -0,0 +1,72 @@
+package casestudy;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class OnlineShoppingTest {
+	WebDriver d;
+  @Test
+  public void f() throws InterruptedException {
+	  System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver_win32 (1)\\chromedriver.exe");
+	  d = new ChromeDriver();
+	  d.get("http://10.232.237.143:443/TestMeApp");
+	  d.manage().window().maximize();
+	  
+	  //register
+	  d.findElement(By.xpath("//a[@href='RegisterUser.htm']")).click();	
+	  d.findElement(By.xpath("//input[@name='userName']")).sendKeys("zxcvbnq");	
+	  d.findElement(By.xpath("//input[@name='firstName']")).sendKeys("zxcvbnq");
+	  d.findElement(By.xpath("//input[@name='lastName']")).sendKeys("zxcvbnq");
+	  d.findElement(By.xpath("//input[@name='password']")).sendKeys("zxcvbnq");
+	  d.findElement(By.xpath("//input[@name='confirmPassword']")).sendKeys("zxcvbnq");
+	  d.findElement(By.xpath("//input[@value='Male']")).click();
+	  d.findElement(By.xpath("//input[@name='emailAddress']")).sendKeys("zxcvbnm@gmail.com");
+	  d.findElement(By.xpath("//input[@name='mobileNumber']")).sendKeys("2222222222");
+	  d.findElement(By.xpath("//input[@name='dob']")).sendKeys("07/02/2019");
+	  d.findElement(By.xpath("//textarea[contains(text(),'Address')]")).sendKeys("22hgdfukkgrdfuhiuw");
+	  d.findElement(By.xpath("//input[@name='answer']")).sendKeys("fdhtejuet");
+	  d.findElement(By.xpath("//input[@name='Submit']")).click();
+	  
+	  //login
+	  
+	  d.findElement(By.xpath("//a[@href='login.htm']")).click();
+	  d.findElement(By.xpath("//input[@name='userName']")).sendKeys("zxcvbnq");
+	  d.findElement(By.xpath("//input[@name='password']")).sendKeys("zxcvbnq");
+	  d.findElement(By.xpath("//input[@name='Login']")).click();
+	  
+	  //search
+	  d.findElement(By.xpath("//*[@id=\"menu3\"]/li[2]/a/span")).click();
+	  Actions a=new Actions(d);
+	  d.findElement(By.xpath("//span[contains(text(),'Home Appliances')]")).click();
+	  Thread.sleep(5000);
+	  d.findElement(By.xpath("//span[contains(text(),'Floor')]")).click(); 
+	   
+	  // add to cart
+	  d.findElement(By.xpath("//a[contains(text(),' Add to cart')]")).click();
+	  d.findElement(By.xpath("//a[@href='displayCart.htm']")).click();  
+	  d.findElement(By.xpath("//a[@href='checkout.htm']")).click();
+	  d.findElement(By.xpath("//textarea[@name='ShippingAdd']")).sendKeys("tehury6hyghr");
+	  d.findElement(By.xpath("//input[@value='Proceed to Pay']")).click();
+	  Thread.sleep(20000);
+	  //bank details
+	  d.findElement(By.xpath("//label[contains(text(),'Andhra Bank')]")).click();
+	  d.findElement(By.xpath("//a[contains(text(),'Continue')]")).click();
+	  d.findElement(By.xpath("//input[@name='username']")).sendKeys("123456");
+	  d.findElement(By.xpath("//input[@name='password']")).sendKeys("Pass@456");
+	  d.findElement(By.xpath("//input[@value='LOGIN']")).click();
+	  Thread.sleep(5000);
+	  d.findElement(By.xpath("//input[@name='transpwd']")).sendKeys("Trans@456");
+	  d.findElement(By.xpath("//input[@value='PayNow']")).click();
+	  
+	  //logout
+	  d.findElement(By.xpath("//a[@href='logout.htm']")).click();
+	  d.close();
+	  
+  }
+}
